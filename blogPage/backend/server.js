985f8320cacc0f453ee917059aa146d459b9e028
kimyork32 <CaronteGG.20851@gmail.com>
2025-10-01 00:08:36 -0500

    cambiando puerto de backend

--- blogPage/backend/server.js
+++ blogPage/backend/server.js
@@ -57,7 +57,7 @@ app.get('/api/article', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
